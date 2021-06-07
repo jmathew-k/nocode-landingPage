5bb550fb25df12ccd993c463e64756488f070105
Joshua Mathew <JoshuaKMathew@gmail.com>
2021-06-07 16:57:00 +0530

    test commit 2

--- src/partials/CTA.js
+++ src/partials/CTA.js
@@ -4,23 +4,25 @@ function CTA() {
   return (
     <section>
 
-          {/* CTA box */}
-          < div className=" bg-white text-center py-32 shadow-md">
+    {/* CTA box */}
+    < div className=" bg-white text-center py-16 px-6 shadow-md">
 
-              {/* CTA content */}
-              {/* <div className="text-center py-40"> */}
-                <p className="h2 text-gray-900 mb-6">Ready to build with Tesseract?</p>
-                <p className="text-gray-900 text-lg mb-6">Give your team the tools required for digital transformation</p>
-              {/* </div> */}
-              <br/>
-              <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full mb-4 sm:w-auto sm:mb-0" href="/demo">Book a Demo</a>
+        {/* CTA content */}
+        {/* <div className="text-center py-40"> */}
+          <p className="h2 text-gray-900 mb-6">Ready to build with Tesseract?</p>
+          <p className="text-gray-900 text-lg mb-6">Give your team the tools required for digital transformation</p>
+        {/* </div> */}
+        <br/>
+        <a className="btn text-white bg-gray-900 hover:bg-gray-800 mb-6 w-full sm:w-auto sm:mb-0" href="/demo">Book a Demo</a>
 
-          </div>
-          
- 
-  
+    </div>
+    
+
+
     </section>
-  );
+
+
+	)
 }
 
 export default CTA;
--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -6,60 +6,40 @@ function HeroHome() {
 
 
   return (
-		<section className='relative'>
-			<div className='max-w-6xl mx-auto px-4 sm:px-6'>
-				{/* Hero content */}
-				<div className='pt-32 pb-12 md:pt-40 md:pb-20'>
-					{/* Section header */}
-					<div className='text-center pb-12 md:pb-16'>
-						<h1
-							className='text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
-							data-aos='zoom-y-out'
-						>
-							Lego Blocks for{' '}
-							<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
-								Automation
-							</span>
-						</h1>
-						<div className='max-w-3xl mx-auto'>
-							<p
-								className='text-xl text-gray-600 mb-8'
-								data-aos='zoom-y-out'
-								data-aos-delay='150'
-							>
-								Build powerful applications with your own business logic in a
-								few clicks
-							</p>
-							<div
-								className='max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center'
-								data-aos='zoom-y-out'
-								data-aos-delay='300'
-							>
-								<div>
-									<a
-										className='btn text-white bg-teal-911 hover:bg-teal-400 w-full mb-4 sm:w-auto sm:mb-0'
-										href='/demo'
-									>
-										Book a Demo
-									</a>
-								</div>
-							</div>
+    <section className="relative">
 
-							<div className='flex flex-col items-center p-12 md:p-6'>
-								<div className='relative'>
-									<img
-										className='justify-items-center'
-										src={require('../images/sphere_gradient.png').default}
-										alt='sphere'
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Section header */}
+          <div className="text-center pb-12 md:pb-6">
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Lego Blocks for <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Automation</span></h1>
+            <div className="max-w-3xl mx-auto">
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Build powerful applications with your own business logic in a few clicks</p>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+                <div>
+                  <a className="btn text-white bg-teal-911 hover:bg-teal-400 w-full mb-4 sm:w-auto sm:mb-0" href="/demo">Book a Demo</a>
+                </div>
+
+              </div>
+                <div className="w-full">
+                  <img className="" src={require('../images/sphere_gradient.png').default} alt="sphere" />
+                </div>
+              </div>
+
+
+          </div>
+          </div>
+
+
+        </div>
+
+      
+    </section>
+  );
 }
 
 export default HeroHome;
+
