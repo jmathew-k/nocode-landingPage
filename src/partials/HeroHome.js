2b828eaee1d6685ed425c7a42207b241a019dc7b
Joshua Mathew <JoshuaKMathew@gmail.com>
2021-06-07 16:09:23 +0530

    test commit

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -6,40 +6,60 @@ function HeroHome() {
 
 
   return (
-    <section className="relative">
+		<section className='relative'>
+			<div className='max-w-6xl mx-auto px-4 sm:px-6'>
+				{/* Hero content */}
+				<div className='pt-32 pb-12 md:pt-40 md:pb-20'>
+					{/* Section header */}
+					<div className='text-center pb-12 md:pb-16'>
+						<h1
+							className='text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
+							data-aos='zoom-y-out'
+						>
+							Lego Blocks for{' '}
+							<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
+								Automation
+							</span>
+						</h1>
+						<div className='max-w-3xl mx-auto'>
+							<p
+								className='text-xl text-gray-600 mb-8'
+								data-aos='zoom-y-out'
+								data-aos-delay='150'
+							>
+								Build powerful applications with your own business logic in a
+								few clicks
+							</p>
+							<div
+								className='max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center'
+								data-aos='zoom-y-out'
+								data-aos-delay='300'
+							>
+								<div>
+									<a
+										className='btn text-white bg-teal-911 hover:bg-teal-400 w-full mb-4 sm:w-auto sm:mb-0'
+										href='/demo'
+									>
+										Book a Demo
+									</a>
+								</div>
+							</div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
-        {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
-          {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Lego Blocks for <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Automation</span></h1>
-            <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Build powerful applications with your own business logic in a few clicks</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn text-white bg-teal-911 hover:bg-teal-400 w-full mb-4 sm:w-auto sm:mb-0" href="/demo">Book a Demo</a>
-                </div>
-              </div>
-
-              <div className="flex flex-col items-center pt-12 md:pt-16">
-                <div className="relative">
-                  <img className="justify-items-center" src={require('../images/sphere_gradient.png').default} width="768" alt="sphere" />
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
-      
-    </section>
-  );
+							<div className='flex flex-col items-center p-12 md:p-6'>
+								<div className='relative'>
+									<img
+										className='justify-items-center'
+										src={require('../images/sphere_gradient.png').default}
+										alt='sphere'
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default HeroHome;
