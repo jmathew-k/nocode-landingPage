9a698946261ab010c1903f02d3ac9e3e29a356aa
Joshua Mathew <JoshuaKMathew@gmail.com>
2021-06-08 16:48:08 +0530

    Cta Alternate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,33 +5,29 @@ import HeroHome from '../partials/HeroHome';
 import FeaturesHome from '../partials/Features';
 import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Planet from '../partials/Planet';
-import CTA from '../partials/CTA';
+import CtaAlternative from '../partials/CtaAlternative'
 import Footer from '../partials/Footer';
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+		<div className='flex flex-col min-h-screen overflow-hidden'>
+			{/*  Site header */}
+			<Header />
 
-      {/*  Site header */}
-      <Header />
+			{/*  Page content */}
+			<main className='flex-grow'>
+				{/*  Page sections */}
+				<HeroHome />
+				<FeaturesHome />
+				<FeaturesBlocks />
+				<Planet />
+				<CtaAlternative />
+			</main>
 
-      {/*  Page content */}
-      <main className="flex-grow">
-
-        {/*  Page sections */}
-        <HeroHome />
-        <FeaturesHome />
-        <FeaturesBlocks />
-        <Planet />
-        <CTA />
-
-      </main>
-
-      {/*  Site footer */}
-      <Footer />
-
-    </div>
-  );
+			{/*  Site footer */}
+			<Footer />
+		</div>
+	)
 }
 
 export default Home;
