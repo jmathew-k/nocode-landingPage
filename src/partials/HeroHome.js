8ea8a8da52c4776d0e2d2983eafacdcd047ce7ae
Joshua K Mathew <32446576+jmathew-k@users.noreply.github.com>
2021-07-23 08:26:24 +0530

    Update HeroHome.js

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -15,9 +15,9 @@ function HeroHome() {
 							className='text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
 							data-aos='zoom-y-out'
 						>
-							Lego Blocks for{' '}
+							Lego Blocks to build {' '}
 							<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
-								App Development
+								 Applications
 							</span>
 						</h1>
 						<div className='max-w-3xl mx-auto'>
