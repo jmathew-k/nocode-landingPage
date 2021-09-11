debf064f0cd08ef622493899d98ce4218ee0aaf4
Joshua K Mathew <32446576+jmathew-k@users.noreply.github.com>
2021-09-11 16:41:39 +0530

    Update HeroHome.js

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -17,7 +17,7 @@ function HeroHome() {
 						>
 							Lego Blocks to build {' '}
 							<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
-							Cognitive Apps
+							AI Apps
 							</span>
 						</h1>
 						<div className='max-w-3xl mx-auto'>
