552b1e310806c741fbf244959cba699248aa5c9e
Joshua K Mathew <32446576+jmathew-k@users.noreply.github.com>
2021-09-11 10:23:19 +0530

    Update HeroHome.js

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -10,14 +10,14 @@ function HeroHome() {
 				{/* Hero content */}
 				<div className='pt-32 pb-12 md:pt-40 md:pb-20'>
 					{/* Section header */}
-					<div className='text-center pb-12 md:pb-5'>
+					<div className='text-center pb-12 md:pb-2'>
 						<h1
 							className='text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
 							data-aos='zoom-y-out'
 						>
 							Lego Blocks to build {' '}
 							<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
-								Cognitive Apps
+							Cognitive Apps
 							</span>
 						</h1>
 						<div className='max-w-3xl mx-auto'>
