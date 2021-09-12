a7ce052f4cfc50889fd2de322bbb7d7b8b5bf7e8
Joshua K Mathew <32446576+jmathew-k@users.noreply.github.com>
2021-09-12 12:26:37 +0530

    Update CtaAlternative.js

--- src/partials/CtaAlternative.js
+++ src/partials/CtaAlternative.js
@@ -25,7 +25,7 @@ function CtaAlternative() {
 									to='/demo'
 									className='btn text-black text-sm bg-white hover:bg-gray-100'
 								>
-									GET EARLY ACCESS
+									Get Early Access
 								</Link>
 							</div>
 						</div>
