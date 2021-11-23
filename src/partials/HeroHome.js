b5d7ccbb33445a3ea9bed98b857c07e9f9931428
Joshua K Mathew <32446576+jmathew-k@users.noreply.github.com>
2021-11-23 19:44:05 +0530

    Update HeroHome.js

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -15,7 +15,7 @@ function HeroHome() {
 							className='text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-8'
 							data-aos='zoom-y-out'
 						>
-							Building the infrastructure for the {' '}
+							Infrastructure for the {' '}
 							<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
 							Decentralized Web
 							</span>
